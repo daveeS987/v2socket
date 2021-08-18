86bb89c29fcbeaa0da568960196b16e42dacfd37
Davee Sok <daveesok@gmail.com>
2021-08-18 11:52:27 -0500

    minor changes with words ours and opponent

--- client.py
+++ client.py
@@ -7,31 +7,31 @@ height = 500
 window = pygame.display.set_mode((width, height))
 
 
-def redrawWindow(window, player, player2):
+def redrawWindow(window, player, opponent):
     window.fill((255, 255, 255))
     player.draw(window)
-    player2.draw(window)
+    opponent.draw(window)
     pygame.display.update()
 
 
 def main():
     run = True
     n = Network()
-    player1 = n.getPlayer()
+    our_self = n.getPlayer()
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
-        # Send player1 to server to get back player2 object
-        player2 = n.send(player1)
+        # Send our_self to server to get back opponent object
+        opponent = n.send(our_self)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
 
-        player1.move()
-        redrawWindow(window, player1, player2)
+        our_self.move()
+        redrawWindow(window, our_self, opponent)
 
 
 main()
